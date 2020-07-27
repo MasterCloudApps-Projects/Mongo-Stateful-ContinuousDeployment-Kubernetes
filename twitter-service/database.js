d9104cff818f6a912577b2246d8e40ebce588d98
Fran <francisco.franco@ethcero.es>
2020-07-27 16:48:43 +0200

    twitter service

---
+++ twitter-service/database.js
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb').MongoClient
+
+const connectionOptions = {
+  reconnectInterval: 3000,
+  reconnectTries: 10
+}
+
+let _db;
+
+async function tryConnect(url, attempts) {
+  try {
+    client =  await MongoClient.connect(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    })
+    _db = client.db()
+    console.log('info', 'Connected to MongoDB')
+  } catch (error) {
+    if(attempts) {
+      console.log('warn', `Failed to connect to MongoDB (${attempts} remaining attempts)`)
+      await new Promise(resolve => setTimeout(tryConnect,connectionOptions.reconnectInterval, url, --connectionOptions.reconnectTries))
+    }else {
+      console.log('error', 'Failed to connect to MongoDB', { error})
+      throw Error('Failed to connect to MongoDB')
+    }
+  }
+}
+
+exports.insertOne = async (document) => {
+   await _db.collection("tweets").insertOne(document)
+}
+
+exports.connect = (url) => {
+   return tryConnect(url, connectionOptions.reconnectTries)
+}
